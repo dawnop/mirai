1dfcb9375a605766469f32df34bfa8e65f5b7a77
dawnop <wyfdggssld@163.com>
2026-04-19 21:59:19 +0800

    Bump version to 0.1.1

--- mirai/__init__.py
+++ mirai/__init__.py
@@ -2,4 +2,4 @@ from .log import logger as _logger  # noqa: F401  — must precede torch imports
 from .decorator import op, module
 from .build import build
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
